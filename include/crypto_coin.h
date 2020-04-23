211397b71237648fdc7bb65ca44c7701527d8b5a
tasmanx <m.kuncevicius@gmail.com>
2020-04-23 22:46:35 +0300

    Change price type to string & update api url to get 20 coins

--- include/crypto_coin.h
+++ include/crypto_coin.h
@@ -7,24 +7,24 @@ class CryptoCoin
 {
 public:
     // constructor
-    CryptoCoin(int rank, std::string symbol, std::string name, int price, float change) : 
+    CryptoCoin(int rank, std::string symbol, std::string name, std::string price, float change) : 
         rank_(rank), symbol_(symbol), name_(name), price_(price), change_(change) {}
 
     // getters
     int rank() { return rank_; }
     std::string symbol() { return symbol_; }
     std::string name() { return name_; }
-    int price() { return price_; }
+    std::string price() { return price_; }
     float change() { return change_; }
 
-    static constexpr const char *API_URL = "https://api.coinranking.com/v1/public/coins?limit=10";
+    static constexpr const char *API_URL = "https://api.coinranking.com/v1/public/coins?limit=20";
 
 private:
     // variables to store coin info
     int rank_ = {};
     std::string symbol_ = {};
     std::string name_ = {};
-    int price_ = {};
+    std::string price_ = {};
     float change_ = {};
 };
 
